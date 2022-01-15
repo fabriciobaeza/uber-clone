f352439699c3c38cdb6391eed7be7b1f6a271b0d
Fabricio Baeza <fabriciobaeza@gmail.com>
2022-01-14 23:54:06 -0500

    Building the MapScreen Component

---
+++ components/Map.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const Map = () => {
+    return (
+        <View>
+            <Text>Iam a map</Text>
+        </View>
+    )
+}
+
+export default Map
+
+const styles = StyleSheet.create({})
--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,15 +1,21 @@
- import React from 'react'
- import { StyleSheet, Text, View } from 'react-native'
- 
- const MapScreen = () => {
-     return (
-         <View>
-             <Text>Here is the map stuff</Text>
-         </View>
-     )
- }
- 
- export default MapScreen
- 
- const styles = StyleSheet.create({})
- 
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import tw from "tailwind-react-native-classnames";
+import Map from "../components/Map";
+import MapView from "react-native-maps";
+
+const MapScreen = () => {
+  return (
+    <View>
+      <Text>Here is the map stuff</Text>
+      <View style={tw`h-1/2`}>
+        <Map />
+      </View>
+      <View style={tw`h-1/2`}></View>
+    </View>
+  );
+};
+
+export default MapScreen;
+
+const styles = StyleSheet.create({});
